e362aad09475bcae03b9971be80e78ecd3ad5fe3
DenisFedash <0501054104d@gmail.com>
2023-12-22 10:54:45 +0200

    added snow in ball

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,6 +2,10 @@
 import React, { useEffect, useState } from "react";
 import { Howl } from "howler";
 import styles from "./style.module.css";
+import ball from "../../../public/images/snow-ball.svg";
+import stand from "../../../public/images/stand.svg";
+import Image from "next/image";
+import Snowfall from "react-snowfall";
 
 const predictions = [
   "Ваше бажання збудеться!",
@@ -23,16 +27,18 @@ const sound = new Howl({
 
 const Game = () => {
   const [prediction, setPrediction] = useState(null);
+  const [snowfallActive, setSnowfallActive] = useState(false);
 
   const handlePredictClick = () => {
     const newPrediction = getRandomPrediction();
     setPrediction(newPrediction);
+    setSnowfallActive(!snowfallActive);
     sound.play(); // Відтворення звуку при кожному кліку
   };
 
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
+    <div className=" ">
+      {/* <main className={styles.main}>
         <h1 className={styles.title}>Гра-передбачення Нового Року</h1>
         <button className={styles.button} onClick={handlePredictClick}>
           Клацніть, щоб отримати передбачення
@@ -42,7 +48,23 @@ const Game = () => {
             <p>{prediction}</p>
           </div>
         )}
-      </main>
+      </main> */}
+      <div>
+        <div className=" relative w-[580px] h-[580px] rounded-full overflow-hidden">
+          {snowfallActive && <Snowfall snowflakeCount={3000} />}
+
+          <Image src={ball} alt="snow-ball" width={580} height={580} />
+        </div>
+        <div className="absolute bottom-[275px] left-0">
+          <Image src={stand} alt="stand" width={571} height={228} />
+        </div>
+        <p class="transform -rotate-180 origin-bottom text-2xl">
+          Зосередьте увагу на своїх мріях!
+        </p>
+      </div>
+      <button onClick={handlePredictClick} type="button" className="mt-[300px]">
+        Запустить снегопад
+      </button>
     </div>
   );
 };
