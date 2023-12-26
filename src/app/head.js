8b9d25ca3b6c1589aaf6684b1799588f25535457
DenisFedash <0501054104d@gmail.com>
2023-12-26 21:26:25 +0200

    fixing

--- src/app/head.js
+++ src/app/head.js
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-      <title>COLAB</title>
+      <title>Prediction Game</title>
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta content="width=device-width, initial-scale=1.0" name="viewport" />
       <meta name="description" content="Prediction Game" />
       <meta name="keywords" content="Prediction Game"></meta>
       <link rel="icon" href="./christmas_three.ico" />
