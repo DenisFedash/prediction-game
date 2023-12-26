684d719e818bdc1f0b9338c98c396e05db7e5a62
DenisFedash <0501054104d@gmail.com>
2023-12-26 15:23:37 +0200

    added mobile version

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -10,6 +10,9 @@ import logo from "../../../public/images/logo.svg";
 import smallDragon from "../../../public/images/small-dragon.svg";
 import dragon from "../../../public/images/dragon.svg";
 import presents from "../../../public/images/presents1.svg";
+import presentsLeft from "../../../public/images/presents-left.svg";
+import presentsRight from "../../../public/images/presents-right.svg";
+import presentsCenter from "../../../public/images/presents-center.svg";
 import balls from "../../../public/images/balls.svg";
 import data from "./data.json";
 
@@ -54,38 +57,47 @@ const Game = () => {
   }, [handleDocumentClick]);
 
   return (
-    <div className="ml-auto mr-auto w-[1440px] h-[1024px] bg-back-img bg-no-repeat bg-cover overflow-hidden">
-      <div className="layout relative ">
-        <div className="flex items-center  ml-24 mt-4">
+    <div className="ml-auto mr-auto layout bg-back-mobile lg:bg-back-img bg-no-repeat bg-cover overflow-hidden">
+      <div className=" relative ">
+        <div className="flex items-center">
           <Image
             src={logo}
             alt="logo"
-            width={220}
-            height={184}
-            className="mr-2.5"
+            width={73}
+            height={27}
+            className="mr-2.5 lg:w-[220px] lg:h-[184px] absolute top-14 left-14 lg:top-[-50px] lg:left-24"
           />
-          <h2 className=" font-scriptorama text-[40px]">вітає</h2>
+          <h2 className=" font-scriptorama text-2xl tracking-[4.8px] lg:text-[40px] absolute top-12 left-[135px] lg:top-5 lg:left-[325px]">
+            вітає
+          </h2>
           <Image
             src={smallDragon}
             alt="small-dragon"
             width={60}
             height={45}
-            className="absolute top-[50px] left-[65px]"
+            className="hidden lg:block absolute lg:top-[50px] lg:left-[65px]"
+          />
+          <Image
+            src={dragon}
+            alt="dragon"
+            width={53}
+            height={71}
+            className="absolute top-[51px] left-2 lg:hidden"
           />
           <Image
             src={balls}
             alt="balls"
-            width={229}
-            height={325}
-            className="absolute -top-4 right-[-50px]"
+            width={124}
+            height={176}
+            className=" lg:w-[229px] lg:h-[325px] absolute top-0 right-[-50px] lg:-top-4"
           />
         </div>
-        <div className="mb-12">
-          <h1 className=" font-scriptorama text-[90px] tracking-[3.2px] text-center">
+        <div className="mt-[85px] lg:mt-[130px] mb-10 lg:mb-24">
+          <h1 className=" font-scriptorama text-2xl lg:text-[90px] tracking-[4.4px] lg:tracking-[3.2px] text-center">
             З Новим 2024 роком!
           </h1>
         </div>
-        <div className="lg:w-[599px] mb-6 ml-24 font-dihjauti text-[32px]">
+        <div className="w-[325px] lg:w-[599px] tracking-[1.6px] lg:tracking-[3.2px] lg:leading-9 mb-[400px] lg:mb-[325px] mx-8 lg:ml-24 font-dihjauti text-base lg:text-[32px]">
           <p className="mb-6 ">
             Нехай здійсняться всі бажання! Нехай це буде рік удачі, любові і
             багатства, рік щастя, перемоги і добра!
@@ -96,22 +108,70 @@ const Game = () => {
             чарівне передбачення на новий рік
           </p>
         </div>
-        <div className="flex justify-between">
-          <Image src={dragon} alt="dragon" width={194} height={261} />
-          <Image src={presents} alt="dragon" width={294} height={294} />
+        <div className="hidden lg:block lg:relative">
+          <Image
+            src={dragon}
+            alt="dragon"
+            width={194}
+            height={261}
+            className=" absolute bottom-0 left-0"
+          />
+          <Image
+            src={presentsRight}
+            alt="dragon"
+            width={294}
+            height={294}
+            className=" absolute right-0 bottom-[-34px]"
+          />
+          <Image
+            src={presentsCenter}
+            alt="presents"
+            width={331}
+            height={349}
+            className="absolute bottom-[-34px] right-[90px]"
+          />
         </div>
-        <div className="absolute lg:top-[250px] lg:right-[200px]">
+        <div className=" lg:hidden relative">
+          <Image
+            src={presentsLeft}
+            alt="presents"
+            width={231}
+            height={249}
+            className="absolute bottom-0 left-0"
+          />
+          <Image
+            src={presentsRight}
+            alt="presents"
+            width={231}
+            height={249}
+            className="absolute bottom-0 right-0"
+          />
+          <Image
+            src={presentsCenter}
+            alt="presents"
+            width={231}
+            height={249}
+            className="absolute bottom-0 left-[70px]"
+          />
+        </div>
+        <div className="absolute top-[450px] right-[50px] lg:top-[250px] lg:right-[200px]">
           <div className="relative overflow-hidden lg:w-[580px] lg:h-[580px] rounded-full ">
             {snowfallActive && <Snowfall snowflakeCount={3000} />}
 
-            <Image src={ball} alt="snow-ball" width={580} height={580} />
+            <Image
+              src={ball}
+              alt="snow-ball"
+              width={287}
+              height={287}
+              className="lg:w-[580px] lg:h-[580px]"
+            />
           </div>
           {currentImage && currentImage === "stand" ? (
-            <div className="absolute lg:top-[400px]">
+            <div className="absolute top-[210px] lg:top-[400px]">
               <div className="relative">
                 <Image src={paper} alt="stand" width={571} height={228} />
-                <p className=" font-dihjauti text-[25px] w-[443px] absolute top-[90px] left-[60px] text-center ">
-                  <p className=" font-scriptorama tracking-widest mb-4 tex-[32px]">
+                <p className=" font-dihjauti text-xs lg:text-[25px] w-[200px] lg:w-[443px] absolute top-[50px] left-[40px] lg:top-[90px] lg:left-[60px] text-center lg:leading-9">
+                  <p className=" font-scriptorama tracking-widest mb-4 lg:mb-10 lg:text-[32px]">
                     Передбачення:
                   </p>
                   {prediction}
@@ -119,12 +179,12 @@ const Game = () => {
               </div>
             </div>
           ) : (
-            <div className="absolute top-[460px] right-[1.5px]">
+            <div className="absolute top-[240px] right-0 lg:top-[460px] lg:right-[1.5px]">
               <div className=" relative">
                 <Image src={stand} alt="stand" width={610} height={228} />
                 <button
                   onClick={handlePredictClick}
-                  className=" bg-green-600 w-[150px] h-[50px] rounded-xl font-dihjauti text-xl focus:outline-none transform transition-transform duration-200 ease-in-out active:scale-90 absolute top-1/2 left-[220px]"
+                  className=" bg-green-600 w-[75px] h-[25px] lg:w-[150px] lg:h-[50px] rounded-md lg:rounded-xl font-dihjauti text-sm lg:text-xl focus:outline-none transform transition-transform duration-200 ease-in-out active:scale-90 absolute top-1/2 left-[110px] lg:left-[220px]"
                 >
                   Тиць!
                 </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,7 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         "back-img": "url('/images/background.jpg')",
+        "back-mobile": "url('/images/back-mobile.jpg')",
       },
     },
   },
