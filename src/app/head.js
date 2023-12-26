9be8b3adf508df31d0c22bae22c2ae55bc83fdd6
DenisFedash <0501054104d@gmail.com>
2023-12-26 15:30:31 +0200

    added favicon

---
+++ src/app/head.js
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>COLAB</title>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Prediction Game" />
+      <meta name="keywords" content="Prediction Game"></meta>
+      <link rel="icon" href="./christmas_three.ico" />
+    </>
+  );
+}
